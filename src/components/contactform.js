2feaceeffea806418c27217db7c6ce65d9c07bdc
Chris Lafortune <chris.lafortune@gmail.com>
2022-04-13 02:02:25 -0400

    revise index netlify form

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,29 +1,13 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
-
-
 export const ContactUs = () => {
-    const form = useRef();
-    
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm('service_d2ux57n', 'template_1bqhreb', form.current, 'dyp1fhF9GDIreN8pd')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-        
-        e.target.reset();
-    }
 
     return (
         <div className='contact-form container'>
             <form name="contact" method="POST">
-                <input type="hidden" name="form-name" value="contact" />
                 <div className='row pt-5 mx-auto'>
+                <input type="hidden" name="form-name" value="contact" />
                     <div class="hidden">
                     <label>
                         Don’t fill this out if you’re human: <input name="bot-field" />
@@ -31,19 +15,19 @@ export const ContactUs = () => {
                     </div>
                     <div className='col-8 form-group mx-auto'>
                         <label for="from_name">Name *</label>
-                        <input required type="text" className='form-control' name='name' id='name'/>
+                        <input required type="text" className='form-control' name="name" id='name'/>
                     </div>
                     <div className='col-8 form-group pt-2 mx-auto'>
                         <label for="reply_to">Email Address *</label>
-                        <input required type="email" className='form-control' name='email' id='email'/>
+                        <input required type="email" className='form-control' name="email" id='email'/>
                     </div>
                     <div className='col-8 form-group pt-2 mx-auto'>
                         <label for="subject">Subject *</label>
-                        <input required type="text" className='form-control' name='subject' id='subject'/>
+                        <input required type="text" className='form-control' name="subject" id='subject'/>
                     </div>
                     <div className='col-8 form-group pt-2 mx-auto'>
                         <label for="message">Message *</label>
-                        <textarea required rows='10' type="text" className='form-control' name='message' id='message'/>
+                        <textarea required rows='10' type="text" className='form-control' name="message" id='message'/>
                     </div>
                     <div data-netlify-recaptcha="true"></div>
                     <div className='col-8 pt-3 mx-auto'>
