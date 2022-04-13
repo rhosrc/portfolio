f0fab63cb2f6b7f183920283fe8fa4f29de8b921
Chris Lafortune <chris.lafortune@gmail.com>
2022-04-13 02:27:12 -0400

    revise contactform component

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -6,25 +6,25 @@ export const ContactUs = () => {
     return (
         <div className='contact-form container'>
             <form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
                 <div className='row pt-5 mx-auto'>
-                <input type="hidden" name="form-name" value="contact" />
+                
                     <div className='col-8 form-group mx-auto'>
                         <label for="from_name">Name *</label>
-                        <input required type="text" className='form-control' name="name" id='name'/>
+                        <input required type="text" className='form-control' name="name"/>
                     </div>
                     <div className='col-8 form-group pt-2 mx-auto'>
                         <label for="reply_to">Email Address *</label>
-                        <input required type="email" className='form-control' name="email" id='email'/>
+                        <input required type="email" className='form-control' name="email"/>
                     </div>
                     <div className='col-8 form-group pt-2 mx-auto'>
                         <label for="subject">Subject *</label>
-                        <input required type="text" className='form-control' name="subject" id='subject'/>
+                        <input required type="text" className='form-control' name="subject"/>
                     </div>
                     <div className='col-8 form-group pt-2 mx-auto'>
                         <label for="message">Message *</label>
-                        <textarea required rows='10' type="text" className='form-control' name="message" id='message'/>
+                        <textarea required rows='10' type="text" className='form-control' name="message"/>
                     </div>
-                    <div data-netlify-recaptcha="true"></div>
                     <div className='col-8 pt-3 mx-auto'>
                         <input type="submit" className='btn gray' value='Send Message'/>
                     </div>
